cbfc18953b7d57a4bfbac49615d5d8cffc6c0de0
Harsh Kashyap <harshkashyap307@gmail.com>
2020-10-31 17:47:49 +0530

    Add navbar

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Hi there!!!</h1>
+    <Navbar />
     </div>
+    </BrowserRouter>
   );
 }
 
