5fbdf86d7e6f0d269b06819158b7e4e5c7ccc3aa
Harsh Kashyap <harshkashyap307@gmail.com>
2020-11-02 13:33:54 +0530

    Add Login and Sign Up Forms

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
+
   return (
     <BrowserRouter>
-    <div className="App">
-    <Navbar />
-    </div>
+      <div className="app">
+        <Navbar />
+        <Switch>
+          <Route exact path = '/' component={Dashboard} />
+          <Route path = '/signin' component={SignIn} />
+          <Route path = '/signup' component = {SignUp} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
